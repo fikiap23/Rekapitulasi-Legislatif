c2e151f8dcc11880626bd56cfeeb3b939e53ce8e
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-14 12:03:33 +0700

    add dapil

--- backend/routes/party.routes.js
+++ backend/routes/party.routes.js
@@ -4,6 +4,14 @@ import { protectAdminRoute } from '../middlewares/protectRoute.js'
 
 const router = express.Router()
 
+// Route for bulk party creation
 router.post('/bulk', protectAdminRoute, partyController.createBulkParties)
+
+// New route for bulk dapil creation per party
+router.post(
+  '/bulkDapils/:partyId',
+  protectAdminRoute,
+  partyController.createBulkDapilForParty
+)
 
 export default router
