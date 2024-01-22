7c51f14f52a9e0e86f3e963303d7fe468bf9e899
Hiyaa <ezra4554@gmail.com>
2024-01-22 15:32:22 +0700

    add feature export pdf

--- frontend/src/sections/kecamatan/view/kecamatan-view.jsx
+++ frontend/src/sections/kecamatan/view/kecamatan-view.jsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+import JSPdf from 'jspdf';
+import html2canvas from 'html2canvas';
+import { useRef, useState, useEffect } from 'react';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -7,6 +9,7 @@ import {
   Paper,
   Table,
   Stack,
+  Button,
   TableRow,
   TableBody,
   TableCell,
@@ -18,10 +21,11 @@ import {
 
 import resultService from 'src/services/resultService';
 
+import Iconify from 'src/components/iconify/iconify';
+
 import KecamatanSearch from '../kecamatan-search';
 import PieChart from '../../../layouts/dashboard/common/pie-chart';
 import BarChart from '../../../layouts/dashboard/common/bar-chart';
-
 // ----------------------------------------------------------------------
 const rowsPerPageOptions = [10, 15, 30];
 export default function KecamatanView() {
@@ -69,6 +73,48 @@ export default function KecamatanView() {
     setPage(0);
   };
 
+  // print area function
+  const handlePrint = () => {
+    if (selectedKecamatanName) {
+      const input = pdfRef.current.querySelectorAll('.printArea');
+      html2canvas(input[0]).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new JSPdf('p', 'mm', 'a4', true);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = pdf.internal.pageSize.getHeight();
+        const imgWidth = canvas.width;
+        const imgHeight = canvas.height;
+        const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+        const imgX = (pdfWidth - imgWidth * ratio) / 2;
+        const imgY = 10;
+
+        // Page 1
+        pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+
+        pdf.addPage();
+
+        // Page 2
+        const secondPageX = 10;
+        const secondPageY = 10;
+
+        // Capture and add the second grid to the second page
+        html2canvas(input[1]).then((secondGridCanvas) => {
+          pdf.addImage(
+            secondGridCanvas.toDataURL('image/png'),
+            'PNG',
+            secondPageX,
+            secondPageY,
+            imgWidth * ratio,
+            imgHeight * ratio
+          );
+          // Save the PDF
+          pdf.save(`Data_${selectedKecamatanName}.pdf`);
+        });
+      });
+    }
+  };
+  const pdfRef = useRef();
+
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" mb={5}>
@@ -80,62 +126,69 @@ export default function KecamatanView() {
           <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
             <KecamatanSearch kecamatans={kecamatans} onSelectKecamatan={handleSelectKecamatan} />
           </Stack>
+          <Button
+            onClick={() => handlePrint()}
+            variant="contained"
+            startIcon={<Iconify icon="fa6-solid:file-pdf" />}
+          >
+            Export Data
+          </Button>
 
-          <Grid container spacing={3}>
-            <Grid xs={12} md={6} lg={8}>
-              <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Kelurahan</TableCell>
-                      <TableCell align="right">Total Suara</TableCell>
-                      <TableCell align="right">Suara Sah</TableCell>
-                      <TableCell align="right">Suara Tidak Sah</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {kelurahans
-                      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                      .map((row) => (
-                        <TableRow
-                          key={row.village_id}
-                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                          <TableCell component="th" scope="row">
-                            {row.village_name}
-                          </TableCell>
-                          <TableCell align="right">{row.total_voters}</TableCell>
-                          <TableCell align="right">{row.total_valid_ballots}</TableCell>
-                          <TableCell align="right">{row.total_invalid_ballots}</TableCell>
-                        </TableRow>
-                      ))}
-                  </TableBody>
-                </Table>
-                <TablePagination
-                  rowsPerPageOptions={rowsPerPageOptions}
-                  component="div"
-                  count={kelurahans.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
+          <Grid container spacing={3} ref={pdfRef}>
+            <Grid container lg={12} className="printArea">
+              <Grid xs={12} md={6} lg={8}>
+                <TableContainer component={Paper}>
+                  <Table aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Kelurahan</TableCell>
+                        <TableCell align="right">Total Suara</TableCell>
+                        <TableCell align="right">Suara Sah</TableCell>
+                        <TableCell align="right">Suara Tidak Sah</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {kelurahans
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => (
+                          <TableRow
+                            key={row.village_id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell component="th" scope="row">
+                              {row.village_name}
+                            </TableCell>
+                            <TableCell align="right">{row.total_voters}</TableCell>
+                            <TableCell align="right">{row.total_valid_ballots}</TableCell>
+                            <TableCell align="right">{row.total_invalid_ballots}</TableCell>
+                          </TableRow>
+                        ))}
+                    </TableBody>
+                  </Table>
+                  <TablePagination
+                    rowsPerPageOptions={rowsPerPageOptions}
+                    component="div"
+                    count={kelurahans.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                  />
+                </TableContainer>
+              </Grid>
+              <Grid xs={12} md={6} lg={4}>
+                <PieChart
+                  title="Perolehan Suara"
+                  chart={{
+                    series: dataParties.map((item) => ({
+                      label: item.name,
+                      value: item.total_votes_party,
+                    })),
+                  }}
                 />
-              </TableContainer>
+              </Grid>
             </Grid>
-
-            <Grid xs={12} md={6} lg={4}>
-              <PieChart
-                title="Perolehan Suara"
-                chart={{
-                  series: dataParties.map((item) => ({
-                    label: item.name,
-                    value: item.total_votes_party,
-                  })),
-                }}
-              />
-            </Grid>
-
-            <Grid xs={12} md={12} lg={12}>
+            <Grid xs={12} md={12} lg={12} className="printArea">
               <BarChart
                 title="Perolehan Suara Per Partai"
                 chart={{
