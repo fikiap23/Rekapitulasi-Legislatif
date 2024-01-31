daac283deb576cb7b20ef0cae04e2a369d6dc61d
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-31 12:13:21 +0700

    add

--- backend/controllers/tpsController.js
+++ backend/controllers/tpsController.js
@@ -259,6 +259,120 @@ const tpsController = {
       })
     }
   },
+
+  getAllTpsResult: async (req, res) => {
+    try {
+      // Find all tps
+      const tps = await Tps.find().select(
+        '_id total_voters valid_ballots_detail'
+      )
+
+      // get all valid ballots from all tps
+      //   const validBallotArray = []
+
+      //   let valid_ballots_detail = await getValidBallotsHelper(result)
+
+      //   // Sum up the total_voters from all tps
+      //   const total_voters = tps.reduce(
+      //     (total, tps) => total + tps.total_voters,
+      //     0
+      //   )
+
+      //   // Combine and aggregate the results
+      //   const aggregatedResult = {
+      //     total_invalid_ballots: result.reduce(
+      //       (total, result) => total + result.total_invalid_ballots,
+      //       0
+      //     ),
+      //     total_valid_ballots: result.reduce(
+      //       (total, result) => total + result.total_valid_ballots,
+      //       0
+      //     ),
+      //     total_voters: total_voters,
+      //   }
+
+      // Return the aggregated result
+      return apiHandler({
+        res,
+        status: 'success',
+        code: 200,
+        message: 'Voting results for all tps retrieved successfully',
+        // data: { ...aggregatedResult, valid_ballots_detail },
+        data: tps,
+        error: null,
+      })
+    } catch (error) {
+      console.error('Error getting total results by district:', error)
+      return apiHandler({
+        res,
+        status: 'error',
+        code: 500,
+        message: 'Internal Server Error',
+        data: null,
+        error: { type: 'InternalServerError', details: error.message },
+      })
+    }
+  },
+}
+
+const getValidBallotsHelper = async (validBallots) => {
+  try {
+    // Memastikan validBallots adalah array
+    const ballotsArray = Array.isArray(validBallots)
+      ? validBallots
+      : [validBallots]
+
+    const totalVotes = ballotsArray.reduce((acc, result) => {
+      for (const party of Object.values(result.valid_ballots_detail)) {
+        const partyId = party.party_id
+
+        if (!acc[partyId]) {
+          acc[partyId] = {
+            party_id: partyId,
+            name: party.name, // Include party name
+            total_votes_party: 0,
+            candidates: {},
+          }
+        }
+
+        for (const candidate of Object.values(party.candidates)) {
+          if (candidate) {
+            const candidateId = candidate.candidate_id
+
+            if (!acc[partyId].candidates[candidateId]) {
+              acc[partyId].candidates[candidateId] = {
+                candidate_id: candidateId,
+                name: candidate.name, // Include candidate name
+                number_of_votes: 0,
+              }
+            }
+
+            acc[partyId].candidates[candidateId].number_of_votes +=
+              candidate.number_of_votes
+            acc[partyId].total_votes_party += candidate.number_of_votes
+          }
+        }
+      }
+
+      return acc
+    }, {})
+
+    const totalVotesTransformed = Object.values(totalVotes).map((party) => ({
+      party_id: party.party_id,
+      name: party.name,
+      total_votes_party: party.total_votes_party,
+      candidates: Object.values(party.candidates).map((candidate) => ({
+        candidate_id: candidate.candidate_id,
+        name: candidate.name,
+        number_of_votes: candidate.number_of_votes,
+      })),
+    }))
+
+    return totalVotesTransformed
+  } catch (error) {
+    console.error('Error getting total results by district:', error)
+    return null
+  }
 }
 
 export default tpsController
--- backend/routes/tps.routes.js
+++ backend/routes/tps.routes.js
@@ -11,4 +11,6 @@ router.post(
   tpsController.fillValidBallotsDetail
 )
 
+router.get('/', tpsController.getAllTpsResult)
+
 export default router
